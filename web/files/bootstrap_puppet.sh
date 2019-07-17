89b2eeeb271226e93324eb81675bf1b0e989fd4d
Skylar Iolta <siolta@users.noreply.github.com>
2019-07-17 11:57:06 -0700

    fixed -d dpkg flag to proper -i

--- web/files/bootstrap_puppet.sh
+++ web/files/bootstrap_puppet.sh
@@ -2,11 +2,12 @@
 
 curl https://apt.puppetlabs.com/puppet-release-xenial.deb -o puppet-release-xenial.deb
 sleep 15
-sudo dpkg -d puppet-release-xenial.deb
+sudo dpkg -i puppet-release-xenial.deb
 sudo apt-get -qq update
 sudo apt-get install -yq puppet-agent
 sudo /opt/puppetlabs/bin/puppet module install puppet-nginx
 sudo /opt/puppetlabs/bin/puppet module install puppetlabs-apt
+sudo mkdir -p /var/www/html
 cat >/tmp/nginx.pp << "EOF"
 class{'nginx': }
 EOF
