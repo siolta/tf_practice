0d0bec55a84ef42d525256915b5b48f8e019710e
Skylar Iolta <siolta@users.noreply.github.com>
2019-07-31 14:03:57 -0700

    remote state for web env

---
+++ web/remote_state/variables.tf
@@ -0,0 +1,20 @@
+variable "profile" {
+  type        = string
+  description = "The AWS credential profile to use."
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS Region to run in."
+}
+
+variable "prefix" {
+  default = "TF-book-siolta"
+  description = "The name of our org, i.e. examplecom"
+}
+
+variable "environment" {
+  default = "web"
+  description = "The name of our environment, i.e. development."
+}
+
