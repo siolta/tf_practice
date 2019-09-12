2df9a9bd504aeaf45d123eae88d6585339d0fe14
Skylar Iolta <siolta@users.noreply.github.com>
2019-09-11 19:21:30 -0700

    updated vpc module interface to match tf_vpc module

--- modules/vpc/interface.tf
+++ modules/vpc/interface.tf
@@ -1,16 +1,69 @@
-variable "name" {
-  type        = string
-  description = "The name of the VPC."
+variable "region" {
+  type = string
+  description = "The AWS region to launch the VPC in."
 }
 
-variable "cidr" {
+variable "environment" {
+  type = string
+  description = "The name of our environment, i.e. development."
+  default = "development"
+}
+
+variable "key_name" {
+  type = string
+  description = "The AWS key pair to use for resources."
+  default = "tfb_key"
+}
+
+variable "vpc_cidr" {
   type        = string
   description = "The CIDR of the VPC."
 }
 
-variable "public_subnet" {
-  type        = string
-  description = "The public subnet to create."
+variable "public_subnets" {
+  type        = list(string)
+  default     = []
+  description = "The public subnets to create."
+}
+
+variable "private_subnets" {
+  type = list(string)
+  default = []
+  description = "The private subnets to create."
+}
+
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+  description = "The host instance type to launch."
+}
+
+variable "ami" {
+  type = map(string)
+  default = {
+    "us-east-1" = "ami-f652979b"
+    "us-west-1" = "ami-7c4b331c"
+    "eu-west-1" = "ami-0ae77879"
+  }
+
+  description = "The (Ubuntu 14.04) AMIs to use for web and app instances."
+}
+
+variable "bastion_instance_type" {
+  type = string
+  default = "t2.micro"
+  description = "The bastion host instance type."
+}
+
+variable "bastion_ami" {
+  type = map(string)
+  default = {
+    "us-east-1" = "ami-f652979b"
+    "us-west-1" = "ami-7c4b331c"
+    "eu-west-1" = "ami-0ae77879"
+  }
+
+  description = "The bastion host AMIs."
 }
 
 variable "enable_dns_hostnames" {
@@ -25,14 +78,44 @@ variable "enable_dns_support" {
   default     = true
 }
 
-output "public_subnet_id" {
-  value = "${aws_subnet.public.id}"
+variable "map_public_ip_on_launch" {
+  type = bool
+  description = "Should be false if you do not want to auto-assign public IP on launch"
+  default = true
 }
 
 output "vpc_id" {
-  value = "${aws_vpc.tfb.id}"
+  value = aws_vpc.environment.id
 }
 
-output "cidr" {
-  value = "${aws_vpc.tfb.cidr_block}"
+output "vpc_cidr" {
+  value = aws_vpc.environment.cidr_block
+}
+
+output "bastion_host_dns" {
+  value = aws_instance.bastion.public_dns
+}
+
+output "bastion_host_ip" {
+  value = aws_instance.bastion.public_ip
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "public_route_table_id" {
+  value = aws_route_table.public.id
+}
+
+output "private_route_table_id" {
+  value = aws_route_table.private.id
+}
+
+output "default_security_group_id" {
+  value = aws_vpc.environment.default_security_group_id
 }
